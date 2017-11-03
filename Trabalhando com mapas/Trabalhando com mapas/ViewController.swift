899a1086d36180df60657c94dc5aa35306a0d2ed
Bruno Lopes de Mello <bruno.guitarpro@gmail.com>
2017-11-02 22:37:18 -0200

    Primeiro push do projeto

---
+++ Trabalhando com mapas/Trabalhando com mapas/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Trabalhando com mapas
+//
+//  Created by Bruno Lopes de Mello on 02/11/2017.
+//  Copyright © 2017 Bruno Lopes de Mello. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapa: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
